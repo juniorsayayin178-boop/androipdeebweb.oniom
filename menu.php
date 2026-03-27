d151e469c4c0bb44cec83f3f4769fe1617e80ef7
juniorsayayin178-boop <juniorsayayin178@gmail.com>
2026-03-26 22:55:03 -0500

    Add files via upload
    nuevos archivos

---
+++ menu.php
@@ -0,0 +1,6 @@
+<div class="nav">
+  <a href="index.php">INICIO</a>
+  <a href="tool.php">HERRAMIENTAS</a>
+  <a href="payment.php">SHOP</a>
+  <a href="suport.php">SOPORTE</a>
+</div>
---
+++ suport.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Soporte</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php include 'menu.php'; ?>
+
+<div class="container">
+  <h1>SOPORTE Y CREACIÓN</h1>
+
+  <div class="card">
+    Contacto por Telegram:<br><br>
+    <a class="btn" href="https://t.me/TU_USUARIO">Ir a Telegram</a>
+  </div>
+</div>
+
+</body>
+</html>
